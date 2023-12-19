24b9ed49666808af95eb8a80dc404cc1066c55a3
r1c0n <reconispog@gmail.com>
2023-12-19 14:38:04 -0800

    add rest of languages

---
+++ src/lang/dutch.js
@@ -0,0 +1,4 @@
+export const dutch = {
+  words: ['Huis', 'Appel', 'Auto', 'Tafel', 'Taart', 'Stoel', 'Bloem', 'Raam', 'Tuin', 'Water'],
+  letters: ['ä', 'Ä', 'ë', 'Ë', 'ï', 'Ï', 'ö', 'Ö', 'ü', 'Ü', 'ÿ', 'Ÿ']
+}
