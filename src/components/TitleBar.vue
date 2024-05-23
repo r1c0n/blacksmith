e2408c150fa19e7ba939e00aa0262940159de568
r1c0n <reconispog@gmail.com>
2024-05-23 14:29:37 -0700

    add titlebar.vue

---
+++ src/components/TitleBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="['title-bar', { inactive: inactive }]">
+    <div class="title-bar-text">{{ title }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    inactive: Boolean
+  }
+}
+</script>
